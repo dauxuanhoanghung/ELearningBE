89b006b49569be32c905dcd69523c3aa8bd7f1aa
dauxuanhoanghung <dxhh2510@gmail.com>
2023-09-25 00:05:10 +0700

    amazon service

---
+++ ELearning/src/main/java/com/dxhh/elearning/services/AmazonS3Service.java
@@ -0,0 +1,8 @@
+package com.dxhh.elearning.services;
+
+import java.io.File;
+
+public interface AmazonS3Service {
+
+    String uploadFile(File file);
+}
---
+++ ELearning/src/main/java/com/dxhh/elearning/services/impl/AmazonS3ServiceImpl.java
@@ -0,0 +1,37 @@
+package com.dxhh.elearning.services.impl;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.dxhh.elearning.services.AmazonS3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.net.URL;
+
+@Service
+public class AmazonS3ServiceImpl implements AmazonS3Service {
+
+    @Value("${aws.s3.bucket.name}")
+    private String bucketName;
+    private final AmazonS3 amazonS3;
+    @Autowired
+    public AmazonS3ServiceImpl(AmazonS3 amazonS3) {
+        this.amazonS3 = amazonS3;
+    }
+
+    @Override
+    public String uploadFile(File file) {
+        String path = String.format("images/%s",  file.getName());
+
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, path, file);
+        PutObjectResult result = amazonS3.putObject(putObjectRequest);
+
+        URL s3Url = amazonS3.getUrl(bucketName, path);
+        file.delete();
+
+        return s3Url.toString();
+    }
+}
